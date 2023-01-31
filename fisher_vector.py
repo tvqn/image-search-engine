498424c313718ec2852ee440dc0659a2e084e346
Quang <qtrv698@gmail.com>
2023-01-31 16:38:13 +0700

    feature: add query use fisher vector and cosin

--- fisher_vector.py
+++ fisher_vector.py
@@ -8,6 +8,7 @@ from sklearn.mixture import GaussianMixture
 import cv2
 from tqdm import tqdm
 import h5py
+from PIL import Image
 
 K = 128
 N = 256 * 1000
@@ -60,46 +61,39 @@ def ToImageOpenCV(image, grayScale=False):
         return cv2.cvtColor(npImage, cv2.COLOR_RGB2GRAY)
     return cv2.cvtColor(npImage, cv2.COLOR_RGB2BGR)
 
-def SiftFeatureExtraction(pil_image, imageID, savePath):
+def SiftFeatureExtraction(pil_image, imageID, savePath = None):
     sift = cv2.xfeatures2d.SIFT_create()
     image = ToImageOpenCV(image=pil_image, grayScale=True)
     height, width = image.shape[:2]
     img_resize = cv2.resize(image, (int(0.5*width), int(0.5*height)))
     kp, des = sift.detectAndCompute(img_resize, None)
     
-    with open(os.path.join(savePath, imageID + '.opencv.sift'), 'w') as fileSiftFeature:
-        if des is None:
-            fileSiftFeature.write(str(128) + '\n')
-            fileSiftFeature.write(str(0) + '\n')
-        elif len(des) > 0:
-            fileSiftFeature.write(str(128) + '\n')
-            fileSiftFeature.write(str(len(kp)) + '\n')
-            for j in range(len(des)):
-                locs_str = '0 0 0 0 0 '
-                descs_str = " ".join([str(int(value)) for value in des[j]])
-                all_strs = locs_str + descs_str
-                fileSiftFeature.write(all_strs + '\n')
+    if savePath is not None:
+        with open(os.path.join(savePath, imageID + '.opencv.sift'), 'w') as fileSiftFeature:
+            if des is None:
+                fileSiftFeature.write(str(128) + '\n')
+                fileSiftFeature.write(str(0) + '\n')
+            elif len(des) > 0:
+                fileSiftFeature.write(str(128) + '\n')
+                fileSiftFeature.write(str(len(kp)) + '\n')
+                for j in range(len(des)):
+                    locs_str = '0 0 0 0 0 '
+                    descs_str = " ".join([str(int(value)) for value in des[j]])
+                    all_strs = locs_str + descs_str
+                    fileSiftFeature.write(all_strs + '\n')
     
     return des
-                
-def TrainGaussianMixture(sample, n_components = 128, saveModelPath = None):
-    gmm = GaussianMixture(n_components= n_components, covariance_type='diag')
-    gmm.fit(sample)
-    if saveModelPath is None:
-        saveModelPath = os.path.join(os.getcwd(), "tmp", "gmm.pkl")
-    SaveGaussianMixture(gmm, saveModelPath)
-    
-    return gmm
 
-def SaveGaussianMixture(gmm, savePath):
-    with open(savePath, 'wb') as file:
-        pickle.dump(gmm, file)
+def LoadSiftFeature(siftFeaturePath):
+    hesaff_info = np.loadtxt(siftFeaturePath, skiprows=2)
+    if hesaff_info.shape[0] == 0:
+        hesaff_info = np.zeros((1, 133), dtype = 'float32')
+    elif hesaff_info.shape[0] > 0 and len(hesaff_info.shape) == 1:
+        hesaff_info = hesaff_info.reshape([1, 133])
 
-def LoadGaussianMixture(savePath):
-    loaded_gmm = GaussianMixture(n_components = K, covariance_type='diag')
-    with open(savePath, 'rb') as file:
-        loaded_gmm = pickle.load(file)
-    return loaded_gmm
+    image_desc = np.sqrt(hesaff_info[:, 5:])
+
+    return image_desc
 
 def LoadSiftFeatureStack(siftFeaturePath, randonSeed = 1024):
     # create stack vector of sift feature
@@ -127,16 +121,24 @@ def LoadSiftFeatureStack(siftFeaturePath, randonSeed = 1024):
     # until now sample was in uint8. Convert to float32
     return sample.astype('float32')
 
-def LoadSiftFeature(siftFeaturePath):
-    hesaff_info = np.loadtxt(siftFeaturePath, skiprows=2)
-    if hesaff_info.shape[0] == 0:
-        hesaff_info = np.zeros((1, 133), dtype = 'float32')
-    elif hesaff_info.shape[0] > 0 and len(hesaff_info.shape) == 1:
-        hesaff_info = hesaff_info.reshape([1, 133])
+def TrainGaussianMixture(sample, n_components = 128, saveModelPath = None):
+    gmm = GaussianMixture(n_components= n_components, covariance_type='diag')
+    gmm.fit(sample)
+    if saveModelPath is None:
+        saveModelPath = os.path.join(os.getcwd(), "tmp", "gmm.pkl")
+    SaveGaussianMixture(gmm, saveModelPath)
+    
+    return gmm
 
-    image_desc = np.sqrt(hesaff_info[:, 5:])
+def SaveGaussianMixture(gmm, savePath):
+    with open(savePath, 'wb') as file:
+        pickle.dump(gmm, file)
 
-    return image_desc
+def LoadGaussianMixture(savePath):
+    loaded_gmm = GaussianMixture(n_components = K, covariance_type='diag')
+    with open(savePath, 'rb') as file:
+        loaded_gmm = pickle.load(file)
+    return loaded_gmm
 
 def ExtractionFeature(siftFeature, pca_transform = None, gmm = None):
     mean = gmm.means_
@@ -164,6 +166,12 @@ def PCA_Transform(sample, save = False, savePath = None):
     return np.dot(sample, pca_transform)
 
 def BuildFisherVectorDict(config: dict):
+    for item in tqdm(os.listdir(config['dataset']), desc="Creat sift_feature"):
+        imageID = item.split(".jpg")[0]
+        imagePath = os.path.join(config["dataset"], item)
+        image = Image.open(imagePath)
+        SiftFeatureExtraction(pil_image = image, imageID = imageID, savePath = config['siftFeaturePath'])
+
     sampleStack = LoadSiftFeatureStack(siftFeaturePath= config['siftFeaturePath'])
     sampleStack = PCA_Transform(sample= sampleStack)
 
@@ -196,8 +204,54 @@ def BuildFisherVectorDict(config: dict):
     h5f['names'] = imgNames
     h5f.close()
 
-def Query():
-    pass
+def compute_cosin_distance(Q, feats, names):
+    """
+    feats and Q: L2-normalize, n*d
+    """
+    dists = np.dot(Q, feats.T)
+    idxs = np.argsort(dists)[::-1]
+    rank_dists = dists[idxs]
+    rank_names = [names[k] for k in idxs]
+    return (idxs, rank_dists, rank_names)
+
+def compute_euclidean_distance(Q, feats, names, k = None):
+    if k is None:
+        k = len(feats)
+
+    dists = ((Q - feats)**2).sum(axis=1)
+    idx = np.argsort(dists) 
+    dists = dists[idx]
+    rank_names = [names[k] for k in idx]
+
+    return (idx[:k], dists[:k], rank_names)
+
+def reranking(Q, data, inds, names, top_k = 50):
+    vecs_sum = data[0, :]
+    for i in range(1, top_k):
+        vecs_sum += data[inds[i], :]
+    vec_mean = vecs_sum/float(top_k)
+    Q = normalize(Q - vec_mean)
+    for i in range(top_k):
+        data[i, :] = normalize(data[i, :] - vec_mean)
+    sub_data = data[:top_k]
+    sub_idxs, sub_rerank_dists, sub_rerank_names = compute_cosin_distance(Q, sub_data, names[:top_k])
+    names[:top_k] = sub_rerank_names
+    return 
+
+def Query(image, config):
+    h5f = h5py.File(config["featureSavePath"] , 'r')
+    feats = h5f['feats']
+    names = list(h5f['names'])
+    
+    # Extract feature of image
+    gmm = LoadGaussianMixture(config['gmmSavePath'])
+    sift = np.sqrt(SiftFeatureExtraction(image, imageID = imageID, savePath = siftFeaturePath))
+    sift = PCA_Transform(sample= sift)
+    feature = FisherVectorExtraction(sift, gmm)
+    # Query
+    idxs, rank_dists, rank_names = compute_euclidean_distance(feature, feats, names, 1)
+
+    print([name for name in rank_names])
 
 def main():
     # Short demo.
@@ -217,11 +271,22 @@ if __name__ == '__main__':
         "n_components" : 128,
         "n_sample" : 256 * 1000,
         "trainGMM": False,
+        "dataset": os.path.join(os.getcwd(), "data"),
         "siftFeaturePath": os.path.join(os.getcwd(), "sift_feature"),
         "gmmSavePath": os.path.join(os.getcwd(), "tmp", "gmm.pkl"),
         "featureSavePath": os.path.join(os.getcwd(), "tmp", 'fisher.h5')
     }
 
     BuildFisherVectorDict(config)
+    # siftFeaturePath = os.path.join(os.getcwd(), "sift_feature", "all_souls_000001.opencv.sift")
+    # load_sift = LoadSiftFeature(siftFeaturePath)
+    # print(load_sift.shape)
+    # hesaff_info = np.loadtxt(siftFeaturePath, skiprows=2)
+    # print(hesaff_info.shape)
+    # imageID = "all_souls_000002"
+    # siftFeaturePath = os.path.join(os.getcwd(), "sift_feature")
+    # imagePath = os.path.join(os.getcwd(), "data", imageID + ".jpg")
+    # image = Image.open(imagePath)
 
+    # Query(image= image, config= config)
     # pdb.set_trace()
